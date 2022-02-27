fa80e7bf3811acb3b669f68e66d668a5c2ab34e5
LachTomasz <lach.tomasz@gmail.com>
2022-02-27 16:09:45 +0100

    Initial commit

---
+++ src/LotteryArray/LotteryArray.java
@@ -0,0 +1,53 @@
+package LotteryArray;
+
+/**
+ * Ten program demonstruje sposób tworzenia tablicy trójkątnej.
+ * This program demonstrates how to create a triangular array.
+ *
+ * @author Tomek
+ *
+ */
+public class LotteryArray
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        final int NMAX = 10;
+
+        //Tworzenie tablicy trójkątnej.
+        //Create a triangular array.
+        int[][] odds = new int[NMAX+1][];
+
+        for (int n = 0; n <= NMAX; n++)
+            odds[n] = new int[n+1];
+
+        //Zapełnienie tablicy trójkątnej.
+        //Populate a triangular array.
+        for (int n = 0; n < odds.length; n++)
+            for(int k = 0; k < odds[n].length; k++)
+            {
+                /*
+                 * Obliczanie współczynnika dwumianowego n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k).
+                 * Calculation of the binomial coefficient n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k).
+                 */
+                int lotteryOdds = 1;
+                for(int i = 1; i <= k; i++)
+                    lotteryOdds = lotteryOdds * (n-i+1)/i;
+
+                odds[n][k] = lotteryOdds;
+            }
+
+        //Drukowanie tablicy trójkątnej.
+        //Printing a triangular plate.
+        for(int[] row : odds)
+        {
+            for(int odd : row)
+                System.out.printf("%4d", odd);
+
+            System.out.println();
+        }
+    }
+}
